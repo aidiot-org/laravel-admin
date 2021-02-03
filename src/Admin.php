0ef674c57cee0e316bc68f1fcf897599f0f6a52c
davidchen0530 <davidchen0530>
2021-02-03 13:51:25 +0900

    updates

--- src/Admin.php
+++ src/Admin.php
@@ -340,8 +340,10 @@ class Admin
             $router->get('auth/login', $authController.'@getLogin')->name('admin.login');
             $router->post('auth/login', $authController.'@postLogin');
             $router->get('auth/logout', $authController.'@getLogout')->name('admin.logout');
-            $router->get('auth/setting', $authController.'@getSetting')->name('admin.setting');
-            $router->put('auth/setting', $authController.'@putSetting');
+            if (config('admin.routes.auth_setting', true) === true) {
+                $router->get('auth/setting', $authController.'@getSetting')->name('admin.setting');
+                $router->put('auth/setting', $authController.'@putSetting');
+            }
         });
     }
 
